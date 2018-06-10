5613c35dcbccb081814864f0e0a091b27b939d21
dskrenta <dskrenta@gmail.com>
2018-06-09 21:19:15 -0700

    finish setup

--- App.js
+++ App.js
@@ -1,7 +1,11 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { 
+  StyleSheet, 
+  Text, 
+  View 
+} from 'react-native';
 
-export default class App extends React.Component {
+class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
@@ -21,3 +25,5 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
 });
+
+export default App;
