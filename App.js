8dd9d429ea4bb2ab95655ade1328b17fe3b22ab6
dskrenta <dskrenta@gmail.com>
2018-06-10 19:51:21 -0700

    new deps, basic structure

--- App.js
+++ App.js
@@ -1,29 +1,52 @@
 import React from 'react';
-import { 
-  StyleSheet, 
-  Text, 
-  View 
-} from 'react-native';
+import { createStackNavigator } from 'react-navigation';
+import { ApolloProvider } from 'react-apollo';
+import { ApolloLink } from 'apollo-link';
+import { ApolloClient } from 'apollo-client';
+import { HttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+import { onError } from 'apollo-link-error';
 
-class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
+import Restaurants from './App/components/Restaurants/Restaurants';
+import Restaurant from './App/components/Restaurant/Restaurant';
+
+const httpLink = new HttpLink({ uri: 'https://food.act.today/graphql' });
+
+const errorLink = onError(({ networkError, graphQLErrors }) => {
+  if (graphQLErrors) {
+    graphQLErrors.map(({ message, locations, path }) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
     );
   }
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+  if (networkError) console.log(`[Network error]: ${networkError}`);
 });
+
+const link = ApolloLink.from([
+  errorLink,
+  httpLink
+]);
+
+const client = new ApolloClient({
+  link,
+  cache: new InMemoryCache()
+});
+
+const AppNavigator = createStackNavigator(
+  {
+    Restaurants,
+    Restaurant
+  },
+  {
+    initialRouteName: 'Restaurants'
+  }
+);
+
+const App = () => (
+  <ApolloProvider client={client}>
+    <AppNavigator />
+  </ApolloProvider>
+);
 
 export default App;
