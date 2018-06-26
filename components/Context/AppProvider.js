41053238c760d45595f0081efe8953d63733f79a
dskrenta <dskrenta@gmail.com>
2018-06-26 15:11:47 -0700

    added context state provider, consumer HOC

--- components/Context/AppProvider.js
+++ components/Context/AppProvider.js
@@ -1,8 +1,19 @@
 import React from 'react';
 
-const AppContext = React.createContext();
+import { 
+  MAX_CLOSER,
+  MAX_BETTER,
+  MAX_VEG,
+  MAX_MEAT,
+  MAX_KIDS,
+  MAX_CHEAP,
+  MAX_WINE,
+  MAX_COCKTAILS 
+} from '../../utils/constants';
 
-class AppProvider extends React.Component {
+export const AppContext = React.createContext();
+
+export class AppProvider extends React.Component {
   state = {
     // state
     address: '',
@@ -42,28 +53,93 @@ class AppProvider extends React.Component {
       });
     },
     closer: () => {
-      
+      if (this.state.closer >= MAX_CLOSER) {
+        return;
+      }
+  
+      if (this.state.better > 0) {
+        this.setState({
+          closer: 0,
+          better: 0,
+        });
+      } else {
+        this.setState({
+          closer: this.state.closer + 1,
+          better: 0,
+        });
+      }
     },
     better: () => {
-
+      if (this.state.better >= MAX_BETTER) {
+        return;
+      }
+  
+  
+      if (this.state.closer > 0) {
+        this.setState({
+          closer: 0,
+          better: 0,
+        });
+      } else {
+        this.setState({
+          closer: 0,
+          better: this.state.better + 1
+        });
+      }
     },
     veg: () => {
+      if (this.state.better >= MAX_VEG) {
+        return;
+      }
 
+      this.setState({
+        veg: this.state.veg + 1
+      });
     },
     meat: () => {
+      if (this.state.better >= MAX_MEAT) {
+        return;
+      }
 
+      this.setState({
+        meat: this.state.meat + 1
+      });
     },
     kids: () => {
+      if (this.state.better >= MAX_KIDS) {
+        return;
+      }
 
+      this.setState({
+        kids: this.state.kids + 1
+      });
     },
     cheap: () => {
+      if (this.state.better >= MAX_CHEAP) {
+        return;
+      }
 
+      this.setState({
+        cheap: this.state.cheap + 1
+      });
     },
     wine: () => {
+      if (this.state.better >= MAX_WINE) {
+        return;
+      }
 
+      this.setState({
+        wine: this.state.wine + 1
+      });
     },
     cocktails: () => {
+      if (this.state.better >= MAX_COCKTAILS) {
+        return;
+      }
 
+      this.setState({
+        cocktails: this.state.cocktails + 1
+      });
     }
   };
 
@@ -75,5 +151,3 @@ class AppProvider extends React.Component {
     );
   }
 }
-
-export default AppProvider;
---
+++ components/Context/withContext.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import AppContext from './AppProvider';
+
+export function withContext(WrappedComponent) {
+  return (props) => (
+    <AppContext.Consumer>
+      {(context) => (
+        <WrappedComponent 
+          context={context}
+          {...props}
+        />
+      )}
+    </AppContext.Consumer>
+  )
+};
