413aefa7cd13d73d956f1436b8dca77843b77d91
BDesBrisay <bryce@desbrisay.com>
2018-06-26 18:15:45 -0700

    added more filters aners and search box to filter page

--- components/Restaurants/Restaurants.js
+++ components/Restaurants/Restaurants.js
@@ -1,6 +1,7 @@
 import React from 'react';
-import { View, Text, Button, Dimensions, Platform, Animated } from 'react-native';
+import { View, Text, Dimensions, Platform, Animated } from 'react-native';
 import CarouselPager from 'react-native-carousel-pager';
+import { Button, SearchBar } from 'react-native-elements'
 
 import Header from '../Header/Header';
 import RestaurantCard from './RestaurantCard';
@@ -21,8 +22,8 @@ const sampleData = [
     address: '205 Main St, Newport Beach',
     distance: 2.1,
     rating: 5,
-    references: [{siteName: 'Zagat'}],
-    // description: 'Creative rolls & sushi served omakase (chef\'s choice) style in a casual, relaxed environment.'
+    references: [{siteName: 'Zagat'}, {siteName: 'Discover LA'}, {siteName: 'Thrillist'}],
+    description: 'Creative rolls & sushi served omakase (chef\'s choice) style in a casual, relaxed environment.'
   },
   {
     image: 'http://www.thedanimaleats.com/wp-content/uploads/2016/02/%C3%86-TDE-San-Shi-Go-1-768x512.jpg',
@@ -30,7 +31,8 @@ const sampleData = [
     address: '205 Main St, Newport Beach, CA 92661',
     distance: 2.1,
     rating: 5,
-    description: 'Best sushi in Newport Beach with chirashis loaded with a great selection of fish'
+    references: [{siteName: 'Zagat'}, {siteName: 'Discover LA'},],
+    // description: 'Best sushi in Newport Beach with chirashis loaded with a great selection of fish'
   },
   {
     image: 'http://www.thedanimaleats.com/wp-content/uploads/2016/02/%C3%86-TDE-San-Shi-Go-1-768x512.jpg',
@@ -38,10 +40,27 @@ const sampleData = [
     address: '205 Main St, Newport Beach, CA 92661',
     distance: 2.1,
     rating: 5,
-    description: 'Best sushi in Newport Beach with chirashis loaded with a great selection of fish'
+    // description: 'Best sushi in Newport Beach with chirashis loaded with a great selection of fish'
   },
 
 ]
+
+const colors = {
+  closer: [
+    '#e9e9ef',
+    '#def',
+    '#dff',
+    '#cff',
+    '#bff'
+  ],
+  better: [
+    '#e9e9ef',
+    '#ddf',
+    '#cdf',
+    '#bdf',
+    '#9cf'
+  ]
+}
 
 const { height, width } = Dimensions.get('window');
 const cardHeight = height - (Platform.OS === 'ios' ? 150 : 130);
@@ -51,38 +70,41 @@ class Restaurants extends React.Component {
     super(props);
 
     this.Animation = new Animated.Value(0);
-    this.drawerOpen = true;
+
+    this.state = {
+      drawerOpen: true,
+      closer: 0,
+      better: 0
+    }
   }
 
   toggleDrawer = () => {
-    if (this.drawerOpen === false) {
+    if (this.state.drawerOpen === false) {
+      this.setState({drawerOpen: true});
       Animated.timing(
         this.Animation,
         {
           toValue: 0,
           duration: 500
         }
-      ).start(() => {
-        this.drawerOpen = true;
-      });
+      ).start();
     }
     else {
+      this.setState({drawerOpen: false});
       Animated.timing(
         this.Animation,
         {
           toValue: 1,
           duration: 500
         }
-      ).start(() => {
-        this.drawerOpen = false;
-      });
+      ).start();
     }
   }
 
   render() {
-    const interpolation = this.Animation.interpolate({
+    const offset = this.Animation.interpolate({
       inputRange: [0,1],
-      outputRange: [height - 70, 60]
+      outputRange: [height - 80, 70]
     });
 
     return (
@@ -94,9 +116,98 @@ class Restaurants extends React.Component {
             ))}
           </CarouselPager>
         </View>
-        <Animated.View style={[styles.drawer, {transform: [{translateY: interpolation}]}]}>
-          <Button title="Filter" onPress={this.toggleDrawer} />
-          <Text>Drawer Content</Text>
+        <Animated.View style={[styles.drawer, {transform: [{translateY: offset}]}]}>
+          <View style={styles.topButtons}>
+            <Button 
+              title="Closer" 
+              onPress={() => {this.setState({closer: this.state.closer + 1})}}
+              buttonStyle={styles.closerButton}
+              containerViewStyle={{height: 45}}
+              textStyle={{color: '#097'}}
+              backgroundColor={colors.closer[this.state.closer]}
+            />
+            <Button 
+              title={this.state.drawerOpen ? "Filters" : "Close"}
+              onPress={this.toggleDrawer} 
+              buttonStyle={styles.filterButton} 
+              containerViewStyle={{height: 45}}
+              textStyle={{color: '#067'}}
+              backgroundColor="#e9e9ef"
+            />
+            <Button 
+              title="Better"
+              onPress={() => {this.setState({better: this.state.better + 1})}}
+              buttonStyle={styles.betterButton} 
+              containerViewStyle={{height: 45}}
+              textStyle={{color: '#06a'}}
+              backgroundColor={colors.better[this.state.better]}
+            />
+          </View>
+          <View style={styles.searchContain}>
+            <SearchBar 
+              // lightTheme
+              platform="ios"
+              placeholder="What are you looking for?"
+              containerStyle={styles.searchBox}
+              inputContainerStyle={{backgroundColor: 'green'}}
+            />
+          </View>
+          <View style={styles.bottomButtons}>
+            <View style={styles.buttonContain}>
+              <Button 
+                title="Better"
+                buttonStyle={styles.mainButton} 
+                containerViewStyle={styles.buttonContainer}
+                textStyle={{color: '#06a'}}
+                backgroundColor={colors.better[this.state.better]}
+              />
+            </View>
+            <View style={styles.buttonContain}>
+              <Button 
+                title="Better"
+                buttonStyle={styles.mainButton} 
+                containerViewStyle={styles.buttonContainer}
+                textStyle={{color: '#06a'}}
+                backgroundColor={colors.better[this.state.better]}
+              />
+            </View>
+            <View style={styles.buttonContain}>
+              <Button 
+                title="Better"
+                buttonStyle={styles.mainButton} 
+                containerViewStyle={styles.buttonContainer}
+                textStyle={{color: '#06a'}}
+                backgroundColor={colors.better[this.state.better]}
+              />
+            </View>
+            <View style={styles.buttonContain}>
+              <Button 
+                title="Better"
+                buttonStyle={styles.mainButton} 
+                containerViewStyle={styles.buttonContainer}
+                textStyle={{color: '#06a'}}
+                backgroundColor={colors.better[this.state.better]}
+              />
+            </View>
+            <View style={styles.buttonContain}>
+              <Button 
+                title="Better"
+                buttonStyle={styles.mainButton} 
+                containerViewStyle={styles.buttonContainer}
+                textStyle={{color: '#06a'}}
+                backgroundColor={colors.better[this.state.better]}
+              />
+            </View>
+            <View style={styles.buttonContain}>
+              <Button 
+                title="Better"
+                buttonStyle={styles.mainButton} 
+                containerViewStyle={styles.buttonContainer}
+                textStyle={{color: '#06a'}}
+                backgroundColor={colors.better[this.state.better]}
+              />
+            </View>
+          </View>
         </Animated.View>
       </View>
     );
@@ -109,7 +220,72 @@ const styles = {
     bottom: 0,
     height: height - 70,
     width: '100%',
-    backgroundColor: 'blue'
+    backgroundColor: '#e9e9ef'
+  },
+  topButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'nowrap',
+    alignItems: 'center',
+    justifyContent: 'space-between',
+    paddingHorizontal: 20,
+    height: 60,
+    width: '100%'
+  },
+  filterButton: {
+    flex: 1,
+    height: 45,
+    borderRadius: 5,
+  },
+  closerButton: {
+    flex: 1, 
+    height: 45,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#097'
+  },
+  betterButton: {
+    flex: 1, 
+    height: 45,
+    borderRadius: 5,
+    borderWidth: 1,
+    borderColor: '#06a'
+  },
+  searchContain: {
+    height: 60,
+    width: '100%',
+    paddingTop: 10,
+    paddingHorizontal: 25,
+  },
+  searchBox: {
+    backgroundColor: '#e9e9ef', 
+    borderBottomWidth: 0, 
+    borderTopWidth: 0, 
+    paddingHorizontal: 2
+  },
+  bottomButtons: {
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    marginTop: 10,
+    marginHorizontal: 20,
+    marginBottom: 50,
+    flex: 1
+  },
+  buttonContain: {
+    flexGrow: 1,
+    flexBasis: '50%'
+  },
+  buttonContainer: {
+    flexGrow: 1,
+    flexBasis: '33%'
+  },
+  mainButton: {
+    flex: 1,
+    borderWidth: 1,
+    marginHorizontal: 0,
+    marginVertical: 10,
+    borderRadius: 5
   }
 }
 
